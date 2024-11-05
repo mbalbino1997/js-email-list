37bc4f6b2cfd33a77e454ca603109c416895ed8c
mbalbino1997 <rmassimo.balbino@gmail.com>
2024-11-05 16:58:57 +0100

    add

--- js/script.js
+++ js/script.js
@@ -1,9 +1,29 @@
-console.log(axios);
+const emailList=document.getElementById("emailList");
+const btnGenerator=document.getElementById("btnGenerator");
+const num=10;
 
-axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-    .then((res) => {
-        console.log("risposta" , res.data.response);
+function randomEmailGenerator() {
+    emailList.innerHTML="";
+    const fragment= document.createDocumentFragment();
+    const proms= [];
+    for (let i=0; i<num;i++) {
+        const prom =
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then((res) => {
+                const li = document.createElement("li");
+                li.textContent = `Email ${i+1}: ${res.data.response}`
+                fragment.appendChild(li);
+                
+            })
+            .catch((err) => {
+                console.log("Qualcosa è andato storto", err);
+            })
+            proms.push(prom)
+    }
+    Promise.all(proms).then(() => {
+        emailList.appendChild(fragment);
+
     })
-    .catch((err) => {
-        console.log("Qualcosa è andato storto", err);
-    })
+}
+btnGenerator.addEventListener("click",()=>randomEmailGenerator());
+
